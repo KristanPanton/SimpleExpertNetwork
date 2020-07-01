ad689c13ad647c9758d90dc4e7783a7133e4d807
Kristan Panton <kristan.panton@gmail.com>
2020-06-30 23:16:08 -0400

    Prototype implementation of defect knowledge

--- DefectAnalysisExpert.py
+++ DefectAnalysisExpert.py
@@ -1,18 +1,61 @@
 from experta import *
 
-DEFECTS = ['Bridging', 'Insufficient Fill', 'Random Solder Balls', 'Solder Spattering', 'Mid-Chip Solder Balls',
-           'Tombstoning', 'Voiding', 'BGA Head-on-Pillow', 'Grainy Joints']
+# TODO:
+# Add more questions
+# Complete Bridging Rules
+# Create GUI
 
+DEFECTS_LIST = ['Bridging',
+                'Insufficient Fill',
+                'Random Solder Balls',
+                'Solder Spattering',
+                'Mid-Chip Solder Balls',
+                'Tombstoning',
+                'Voiding',
+                'BGA Head-on-Pillow',
+                'Grainy Joints']
 
-def get_option():
-    while True:
-        try:
-            inp = int(input('What type of defect occurred: '))
-            if 0 > inp > len(DEFECTS) + 1:
-                raise ValueError
-            return inp
-        except ValueError:
-            print("Invalid Input!")
+DEFECTS = {
+    'Bridging':
+        {
+            'Causes': {
+                'PCB': [
+                    ('', 'Infrequent solder mask change'),
+                    ('', 'Insufficient PCB support'),
+                    ('', 'Poor gasketing between stencil and PCB')
+                ],
+                'Stencil': [
+                    ('Is the stencil tension tight?', 'Insufficient stencil tension'),
+                    ('Is the stencil undamaged?', 'Damaged stencil'),
+                    ('Has the stencil been cleaned prior?', 'Unclean stencil'),
+                    ('Are the stencil apertures smaller than the pads?', 'Stencil aperture design'),
+                    'Poor gasketing between stencil and PCB'
+                ],
+                'Gasket': [
+                    ('Is there a good seal between the stencil and the PCB?',
+                     'Poor gasketing between stencil and PCB')
+                ],
+                'Screen Printer': [
+                    'Screen printer misalignment',
+                    'Bad separation speed',
+                    ('Are the squeegee blades undamaged?', 'Damaged squeegee blades'),
+                    'Bad/Inaccurate component placement'
+                ],
+                'Squeegee Blades': ['', 'Bad/Inaccurate component placement'],
+                'Reflow Profile': [('', 'Incorrect reflow profile')],
+                'Solder Paste': [
+                    ('', 'Poor solder paste condition'),
+                    ('', 'Dry solder paste')
+                ]
+            }
+        },
+    'Tombstoning': {
+        'Causes': [
+            ('', 'Component body not covering > 50% of both pads')
+        ]
+    }
+}
+
 
 def get_yorn(query):
     while True:
@@ -26,13 +69,20 @@ def get_yorn(query):
             print("Invalid Input!")
 
 
-
 class Defect(Fact):
-    pass
+    cause = Field(str)
 
 
 class Cause(Fact):
     pass
+
+
+class Component(Fact):
+    condition = Field(str)
+
+
+class Attribute(Fact):
+    name = Field(str, mandatory=True)
 
 
 class Action(Fact):
@@ -40,114 +90,35 @@ class Action(Fact):
 
 
 class BridgingRules:
-    @Rule(Action('Investigate'), Defect('Bridging'), Cause('PCB'))
-    def bridge_investigate_pcb(self):
-        response = get_yorn("Have you removed solder masks between adjacent pads? (yes/no) ")
-        self.declare(Fact(remove_solder_masks=response))
-        response = get_yorn("Is the PCB support sufficient? (yes/no) ")
-        self.declare(Fact(pcb_support_sufficient=response))
-        response = get_yorn("Is the gasketing between the PCB and the stencil acceptable? (yes/no) ")
-        self.declare(Fact(pcb_stencil_gasketing_acceptable=response))
-        if response:
-            self.retract(Cause('PCB'))
+    @Rule(Action('Investigate'), Defect(MATCH.name))
+    def investigate(self, name):
+        # Declare likely components and investigate them
+        for key in DEFECTS[name]['Causes']:
+            self.declare(Component(key))
 
-    @Rule(Defect('Bridging'), Cause('Stencil'))
-    def bridge_investigate_stencil(self):
-        response = get_yorn("Is the ")
-
-    @Rule(Defect('Bridging'), Cause('Screen Printer'))
-    def bridge_investigate_screen_printer(self):
-        pass
-
-    @Rule(Defect('Bridging'), Cause('Component Placement'))
-    def bridge_investigate_component_placement(self):
-        pass
-
-    @Rule(Defect('Bridging'), Cause('Reflow Profile'))
-    def bridge_investigate_reflow_profile(self):
-        pass
-
-    @Rule(Defect('Bridging'), Cause('Solder Paste'))
-    def bridge_investigate_solder_paste(self):
+    @Rule(Action('Investigate'), Defect('Bridging'), Component(MATCH.name))
+    def investigate_components_bridging(self, name):
         pass
 
 
 class InsufficientFillRules:
-    @Rule(Defect('Insufficient Fill'), Cause('Stencil'))
-    def ins_fill_investigate_stencil(self):
-        pass
-
-    @Rule(Defect('Insufficient Fill'), Cause('Screen Printer'))
-    def ins_fill_investigate_screen_printer(self):
-        pass
+    pass
 
 
 class InsufficientSolderRules:
-    @Rule(Defect('Insufficient Solder'), Cause('Stencil'))
-    def ins_solder_investigate_istencil(self):
-        pass
-
-    @Rule(Defect('Insufficient Solder'), Cause('Screen Printer'))
-    def ins_solder_investigate_screen_printer(self):
-        pass
-
-    @Rule(Defect('Insufficient Solder'), Cause('Reflow Profile'))
-    def ins_solder_investigate_reflow_profile(self):
-        pass
-
-    @Rule(Defect('Insufficient Solder'), Cause('Solder Paste'))
-    def ins_solder_investigate_solder_paste(self):
-        pass
+    pass
 
 
 class RandomSolderBallsRules:
-    @Rule(Defect('RandomSolderBalls'), Cause('Stencil'))
-    def rsb_investigate_stencil(self):
-        pass
-
-    @Rule(Defect('RandomSolderBalls'), Cause('Reflow Profile'))
-    def rsb_investigate_reflow_profile(self):
-        pass
-
-    @Rule(Defect('RandomSolderBalls'), Cause('PCB Moisture'))
-    def rsb_investigate_pcb_moisture(self):
-        pass
-
-    @Rule(Defect('RandomSolderBalls'), Cause('Solder Paste'))
-    def rsb_investigate_solder_paste(self):
-        pass
+    pass
 
 
 class SolderSpatteringRules:
-    @Rule(Defect('Solder Spattering'), Cause('PCB'))
-    def solder_spatter_investigate_pcb(self):
-        pass
-
-    @Rule(Defect('Solder Spattering'), Cause('Screen Printer'))
-    def solder_spatter_investigate_screen_printer(self):
-        pass
-
-    @Rule(Defect('Solder Spattering'), Cause('Reflow Profile'))
-    def solder_spatter_investigate_reflow_profile(self):
-        pass
+    pass
 
 
 class MidChipSolderBallsRules:
-    @Rule(Defect('Mid-Chip Solder Balls'), Cause('PCB'))
-    def mcsb_investigate_pcb(self):
-        pass
-
-    @Rule(Defect('Mid-Chip Solder Balls'), Cause('Stencil Design'))
-    def mcsb_investigate_stencil_design(self):
-        pass
-
-    @Rule(Defect('Mid-Chip Solder Balls'), Cause('Screen Printer'))
-    def mcsb_investigate_screen_printer(self):
-        pass
-
-    @Rule(Defect('Mid-Chip Solder Balls'), Cause('Reflow Profile'))
-    def mcsb_investigate_reflow_profile(self):
-        pass
+    pass
 
 
 class TombstoningRules:
@@ -168,34 +139,26 @@ class GrainyJointsRules:
 
 class DefectAnalysisExpert(BridgingRules, KnowledgeEngine):
     @DefFacts()
-    def _initial_conditions(self):
-        yield Action('Investigate')
+    def initiate(self):
+        print("This is an expert system for defect analysis.")
+        print("What type of defect occurred?")
+        [print(f"{i}. {defect}") for i, defect in enumerate(DEFECTS_LIST, 1)]
+        while True:
+            try:
+                defect = DEFECTS_LIST[abs(int(input("Enter number: ")) - 1)]
+                print(f"Your choice is {defect}")
+                yield Action('Investigate')
+                yield Defect(defect)
+                break
+            except (ValueError, IndexError):
+                print("Invalid Input!")
 
-    @Rule(Defect('Bridging'))
-    def investigate_bridging(self):
-        self.declare(Cause('PCB'))
-        self.declare(Cause('Stencil'))
-        self.declare(Cause('Screen Printer'))
-        self.declare(Cause('Component Placement'))
-        self.declare(Cause('Reflow Profile'))
-        self.declare(Cause('Solder Paste'))
-
-    @Rule(Defect('Insufficient Fill'))
-    def insufficient_fill(self):
-        self.declare(Cause('Stencil'))
-        self.declare(Cause('Screen Printer'))
+    @Rule(Action('Report'))
+    def report_findings(self):
+        pass
 
 
 engine = DefectAnalysisExpert()
 engine.reset()
-for i, defect in enumerate(DEFECTS, 1):
-    print(f'{i}.', defect)
-print(f'{len(DEFECTS) + 1}.', "Not sure")
-ans = get_option()
-if ans > len(DEFECTS):
-    engine.declare(Action('Investigate'))
-else:
-    engine.declare(Defect(DEFECTS[ans-1]))
-
 watch('RULES', 'FACTS', 'ACTIVATIONS')
 engine.run()
